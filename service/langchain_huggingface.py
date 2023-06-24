bd6a9427a2ee9a581048ffba47bac3ed534691fc
zhanglei36 <zhanglei36@asiainfo.com>
2023-06-24 20:57:52 +0800

    init

---
+++ service/langchain_huggingface.py
@@ -0,0 +1,16 @@
+import os
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "hf_RrVbibcAmWTTTYSMoleizhangPccgWIwXRaKBHigPi"
+from langchain import HuggingFaceHub
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+llm = HuggingFaceHub(repo_id="bigscience/bloom", model_kwargs={"temperature": 1e-10})
+
+question = "When was Google founded?"
+
+rendered_prompt = template.format(question=question)  # render the prompt using string formatting
+llm_result = llm.generate(prompts=[rendered_prompt])  # pass the rendered prompt as a list
+print(llm_result.generations[0][0].text)
